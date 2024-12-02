6359322444fd7aa8f439160e5fe826e0b0ea2930
oonamo <onam75hernandez@gmail.com>
2024-12-02 08:48:11 -0800

    feat(2): solution

--- 2/main.c
+++ 2/main.c
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,14 +12,17 @@ typedef struct
 {
     unsigned long size;
     unsigned long capacity;
-    char **items;
+    int *items;
 } Vec;
 
 Vec *n_vec();
 void delete_vec(Vec *);
-void append(Vec *, char *);
+void append(Vec *, int);
+void consume_line(FILE *);
 
-double solve(char *file);
+bool issafe(Vec *);
+
+int solve(char *file);
 
 int main(int argc, char *argv[])
 {
@@ -28,16 +32,16 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    double result = solve(argv[1]);
+    int result = solve(argv[1]);
 
-    printf("Result: %f", result);
+    printf("Result: %d", result);
 }
 
-double solve(char *file)
+int solve(char *file)
 {
     FILE *fp = fopen(file, "r");
-    Vec* vec = NULL;
-    double result = 0;
+    Vec *vec = NULL;
+    int result = 0;
 
     if (fp == NULL)
     {
@@ -48,11 +52,75 @@ double solve(char *file)
     vec = n_vec();
 
     char io;
-    while ((io = fgetc(fp)) && io != EOF) {
+
+    int lastnum = 0;
+    int num = 0;
+
+    bool hasFirst = false;
+    bool isIncrement = false;
+    bool isDecrement = false;
+
+    while ((io = fgetc(fp)) && io != EOF)
+    {
+        if (!isspace(io))
+        {
+            num = num * 10 + io - '0';
+        }
+        else
+        {
+            if (!hasFirst)
+            {
+                lastnum = num;
+                num = 0;
+                hasFirst = true;
+                continue;
+            }
+
+            int diff = abs(num - lastnum);
+
+            if (!isIncrement && !isDecrement)
+            {
+                isIncrement = num > lastnum;
+                isDecrement = !isIncrement;
+            }
+
+            if ((isIncrement && num <= lastnum) ||
+                (isDecrement && num >= lastnum) || diff > 3 || diff == 0)
+            {
+                lastnum = 0;
+                hasFirst = false;
+                isDecrement = false;
+                isIncrement = false;
+                consume_line(fp);
+            }
+            else
+            {
+                lastnum = num;
+            }
+
+            num = 0;
+        }
+
+        if (io == '\n' && (isIncrement || isDecrement))
+        {
+            num = 0;
+            lastnum = 0;
+            hasFirst = false;
+            isDecrement = false;
+            isIncrement = false;
+            result++;
+        }
     }
 
     delete_vec(vec);
     return result;
+}
+
+void consume_line(FILE *fp)
+{
+    char io;
+    while ((io = fgetc(fp)) != '\n' && io != EOF)
+        ;
 }
 
 Vec *n_vec()
@@ -69,30 +137,24 @@ void delete_vec(Vec *vec)
     if (vec == NULL)
         return;
 
-    for (unsigned long i = 0; i < vec->size; i++)
-    {
-        if (vec->items[i] != NULL)
-            free(vec->items[i]);
-    }
-
+    free(vec->items);
     free(vec);
 }
 
-void append(Vec *vec, char *val)
+void append(Vec *vec, int val)
 {
     if (vec->size <= vec->capacity)
     {
         if (vec->capacity == 0)
         {
             vec->capacity = INIT_CAP;
-            vec->items = malloc(sizeof(char *) * vec->capacity);
+            vec->items = malloc(sizeof(int *) * vec->capacity);
         }
         else
         {
             vec->capacity += CAP_INCREMENT;
-            vec->items = realloc(vec->items, sizeof(char *) * vec->capacity);
+            vec->items = realloc(vec->items, sizeof(int *) * vec->capacity);
         }
     }
     vec->items[vec->size++] = val;
 }
-
